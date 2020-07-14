04c30b059bfe4e2903ceec4a6abe971633fcf57a
peacechen <github@ghpc.33mail.com>
2020-07-13 23:37:09 -0500

    Formatting

--- install-git-hooks.js
+++ install-git-hooks.js
@@ -22,26 +22,25 @@ function installGitHooks(args) {
   return 0;
 }
 
-
 function copyDir(src, dest) {
-	const files = fs.readdirSync(src);
-	for(let file of files) {
-		const current = fs.lstatSync(path.join(src, file));
-		if(current.isDirectory()) {
-			copyDir(path.join(src, file), path.join(dest, file));
-		} else if(current.isSymbolicLink()) {
-			const symlink = fs.readlinkSync(path.join(src, file));
-			fs.symlinkSync(symlink, path.join(dest, file));
-		} else {
-			copy(path.join(src, file), path.join(dest, file));
-		}
-	}
+  const files = fs.readdirSync(src);
+  for (let file of files) {
+    const current = fs.lstatSync(path.join(src, file));
+    if (current.isDirectory()) {
+      copyDir(path.join(src, file), path.join(dest, file));
+    } else if (current.isSymbolicLink()) {
+      const symlink = fs.readlinkSync(path.join(src, file));
+      fs.symlinkSync(symlink, path.join(dest, file));
+    } else {
+      copy(path.join(src, file), path.join(dest, file));
+    }
+  }
 };
 
 function copy(src, dest) {
-	const oldFile = fs.createReadStream(src);
-	const newFile = fs.createWriteStream(dest);
-	oldFile.pipe(newFile);
+  const oldFile = fs.createReadStream(src);
+  const newFile = fs.createWriteStream(dest);
+  oldFile.pipe(newFile);
 };
 
 module.exports = installGitHooks;
